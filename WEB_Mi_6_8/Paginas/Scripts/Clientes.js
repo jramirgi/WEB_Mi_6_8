bd4220422f3942900b5b57a4b7c316d1d658955a
jramirgi <joseramirez8046@correo.itm.edu.co>
2025-05-28 06:47:45 -0500

    Facturacion

--- WEB_Mi_6_8/Paginas/Scripts/Clientes.js
+++ WEB_Mi_6_8/Paginas/Scripts/Clientes.js
@@ -1,13 +1,47 @@
 ﻿var BaseURL = "http://itm2025mie68.runasp.net/";
 jQuery(function () {
-    $("#dvMenu").load("../Paginas/Menu.html");
+    let URL = BaseURL + "api/TipoTelefonos/LlenarCombo";
     LlenarTablaClientes();
+    LlenarComboXServiciosAuth(URL, "#cboTipoTelefono");
 });
+function LlenarTablaTelefonos() {
+    var tabla = new DataTable('#tblTelefonos');
+    tabla.clear().draw();
+    let Documento = $("#txtDocumento").val();
+    let URL = BaseURL + "api/Telefonos/ListadoTelefonosXCliente?Documento=" + Documento;
+    $("#modTelefonosLabel").html("GESTIÓN DE TELÉFONOS - CLIENTE: " + $("#txtNombre").val() + " " + $("#txtPrimerApellido").val() + " " + $("#txtSegundoApellido").val());
+    LlenarTablaXServiciosAuth(URL, "#tblTelefonos");
+}
 function LlenarTablaClientes() {
-    let URL = BaseURL + "api/Clientes/ConsultarTodos";
+    let URL = BaseURL + "api/Clientes/ClientesConTelefonos";
     LlenarTablaXServiciosAuth(URL, "#tblClientes");
 }
-function LlenarTablaTelefonos() {
-    let URL = BaseURL + "api/Clientes/ConsultarTodos";
-    LlenarTablaXServiciosAuth(URL, "#tblTelefonos");
+async function EjecutarComando(Metodo, Funcion) {
+    event.preventDefault();
+    let URL = BaseURL + "api/Telefonos/" + Funcion;
+    const telefono = new Telefono($("#txtCodigo").val(), $("#txtNumero").val(), $("#txtDocumento").val(), $("#cboTipoTelefono").val());
+    await EjecutarComandoServicioAuth(Metodo, URL, telefono);
+    LlenarTablaTelefonos();
+}
+function Editar(Documento, Nombre, PrimerApellido, SegundoApellido, Direccion, Email, FechaNacimiento) {
+    $("#txtDocumento").val(Documento);
+    $("#txtNombre").val(Nombre);
+    $("#txtPrimerApellido").val(PrimerApellido);
+    $("#txtSegundoApellido").val(SegundoApellido);
+    $("#txtDireccion").val(Direccion);
+    $("#txtEmail").val(Email);
+    $("#txtFechaNacimiento").val(FechaNacimiento);
+}
+function EditarTelefono(Codigo, idTipoTelefono, NumeroTelefono) {
+    $("#txtCodigo").val(Codigo);
+    $("#cboTipoTelefono").val(idTipoTelefono);
+    $("#txtNumero").val(NumeroTelefono);
+}
+class Telefono {
+    constructor(Codigo, Numero, Documento, CodigoTipoTelefono) {
+        this.Codigo = Codigo;
+        this.Numero = Numero;
+        this.Documento = Documento;
+        this.CodigoTipoTelefono = CodigoTipoTelefono;
+    }
 }
